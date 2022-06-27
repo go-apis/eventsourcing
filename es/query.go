0f9d0c1cc5db0126de2c0b0ee5e27ba1437f4008
Chris Kolenko <chris@webcanvas.com.au>
2022-06-27 13:54:51 +1000

    Add pagination to query

--- es/query.go
+++ es/query.go
@@ -2,15 +2,26 @@ package es
 
 import (
 	"context"
+	"fmt"
+	"math"
 	"reflect"
 
 	"github.com/contextcloud/eventstore/es/filters"
 )
 
+type Pagination[T any] struct {
+	Limit      int64
+	Page       int64
+	TotalItems int64
+	TotalPages int64
+	Items      []T
+}
+
 type Query[T any] interface {
 	Load(ctx context.Context, id string) (*T, error)
 	Find(ctx context.Context, filter filters.Filter) ([]T, error)
 	Count(ctx context.Context, filter filters.Filter) (int64, error)
+	Pagination(ctx context.Context, filter filters.Filter) (*Pagination[T], error)
 }
 
 type query[T any] struct {
@@ -36,6 +47,34 @@ func (q *query[T]) Find(ctx context.Context, filter filters.Filter) ([]T, error)
 
 func (q *query[T]) Count(ctx context.Context, filter filters.Filter) (int64, error) {
 	return q.unit.Count(ctx, q.aggregateName, filter)
+}
+
+func (q *query[T]) Pagination(ctx context.Context, filter filters.Filter) (*Pagination[T], error) {
+	if filter.Limit == nil {
+		return nil, fmt.Errorf("Limit required for pagination")
+	}
+	if filter.Offset == nil {
+		return nil, fmt.Errorf("Offset required for pagination")
+	}
+
+	totalItems, err := q.unit.Count(ctx, q.aggregateName, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var items []T
+	if err := q.unit.Find(ctx, q.aggregateName, filter, &items); err != nil {
+		return nil, err
+	}
+
+	totalPages := int64(math.Ceil(float64(totalItems) / float64(*filter.Limit)))
+	return &Pagination[T]{
+		Limit:      int64(*filter.Limit),
+		Page:       int64(*filter.Offset) + 1,
+		TotalItems: totalItems,
+		TotalPages: totalPages,
+		Items:      items,
+	}, nil
 }
 
 func NewQuery[T any](unit Unit) Query[T] {
