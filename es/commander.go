491f0895cfefabcbacd835de3950e0c319ac12f3
Chris Kolenko <chris@webcanvas.com.au>
2022-06-27 18:53:49 +1000

    Add commander helper

---
+++ es/commander.go
@@ -0,0 +1,45 @@
+package es
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Commander[T Command] interface {
+	Handle(w http.ResponseWriter, r *http.Request)
+}
+
+type commander[T Command] struct {
+	cli Client
+}
+
+func (c *commander[T]) Handle(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	var cmd T
+	if err := json.NewDecoder(r.Body).Decode(&cmd); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	unit, err := c.cli.NewUnit(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := unit.Dispatch(ctx, cmd); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := unit.Commit(ctx); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func NewCommander[T Command](cli Client) Commander[T] {
+	return &commander[T]{cli: cli}
+}
--- es/example/main.go
+++ es/example/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/contextcloud/eventstore/es/example/aggregates"
 	"github.com/contextcloud/eventstore/es/example/commands"
 	"github.com/contextcloud/eventstore/es/example/sagas"
-	"github.com/contextcloud/eventstore/es/httputil"
+	"github.com/contextcloud/eventstore/es/filters"
 	"github.com/contextcloud/eventstore/es/local/g"
 
 	"github.com/contextcloud/eventstore/es"
@@ -14,6 +15,36 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+func handle[T es.Command](cli es.Client) http.HandlerFunc {
+	commander := es.NewCommander[T](cli)
+	return commander.Handle
+}
+
+func userQueryFunc(cli es.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		unit, err := cli.NewUnit(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		filter := filters.Filter{}
+
+		q := es.NewQuery[aggregates.User](unit)
+		out, err := q.Find(ctx, filter)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(out); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+}
 
 func main() {
 	cfg, err := es.NewConfig(
@@ -35,8 +66,8 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	r.Post("/commands/demo", httputil.NewCommandHandler[commands.CreateUser](cli))
-	r.Get("/users", httputil.NewQueryHandler[aggregates.User](cli))
+	r.Post("/commands/demo", handle[commands.CreateUser](cli))
+	r.Get("/users", userQueryFunc(cli))
 
 	http.ListenAndServe(":3000", r)
 }
